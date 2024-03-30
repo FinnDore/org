a15e383ce13e112937d76e40b13d68c5af4b06e0
Finn <34718806+FinnDore@users.noreply.github.com>
2024-03-30 12:21:04 +0000

    feat: we now extrat auth token from state now

--- apps/relay/src/main.rs
+++ apps/relay/src/main.rs
@@ -4,15 +4,8 @@ mod org_client;
 
 use std::sync::Arc;
 
-use crate::{
-    client_socket::client_handler,
-    game_socket::game_handler,
-    org_client::{TheState},
-};
-use axum::{
-    routing::get,
-    Router,
-};
+use crate::{client_socket::client_handler, game_socket::game_handler, org_client::TheState};
+use axum::{routing::get, Router};
 
 use tokio::sync::RwLock;
 
@@ -26,8 +19,8 @@ async fn main() {
         .route("/game/:org", get(game_handler))
         .with_state(state);
     let port = std::env::var("PORT").unwrap_or("3001".to_string());
-    let host = format!("0.0.0.0:{:}", port);
-    println!("Running server on {:}", host);
+    let host = format!("0.0.0.0:{}", port);
+    println!("Running server on {}", host);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind(host).await.unwrap();
