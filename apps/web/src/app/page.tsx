3f02426aac2711efd2cdba05ea0f23202518f9e8
Finn <34718806+FinnDore@users.noreply.github.com>
2024-03-31 16:40:16 +0100

    revert

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
-import { type SceneItem } from "@/server/api/routers/scene";
 import { api } from "@/trpc/react";
-import { Canvas } from "@react-three/fiber";
-import { useEffect, useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 import { Group } from "three";
 
 export default function Home() {
@@ -19,78 +18,23 @@ export default function Home() {
     );
 }
 
-function useWebsocket(opts: {
-    onMessage: (data: {
-        objectId: string;
-        path: string;
-        value: number;
-    }) => void;
-    orgName: string;
-}) {
-    useEffect(() => {
-        const ws = new WebSocket(
-            `ws://localhost:3002/sub/${encodeURIComponent(opts.orgName)}`,
-        );
-        ws.onopen = () => {
-            console.log(
-                "%cWebsocket connected",
-                "background:#1fde7c;padding:0.5rem",
-            );
-        };
-        ws.onmessage = (event) => {
-            const payload = JSON.parse(event.data);
-            // console.log(
-            //     "%cMessage",
-            //     "background:#A001fF;padding:0.5rem",
-            //     payload,
-            // );
-            opts.onMessage?.(payload);
-        };
-        ws.onclose = () => {
-            console.log(
-                "%cWebscoket disconnected",
-                "background:#de1f2e;padding:0.5rem",
-            );
-        };
-        return () => {
-            ws.close();
-        };
-    }, [opts.orgName]);
-}
-
 function Scene() {
     const scene = api.scene.getSceneByOrg.useQuery({ orgName: "test" });
-    const [sceneItems, setState] = useState<SceneItem[] | undefined>(
-        scene.data?.items,
-    );
-
-    useEffect(() => {
-        setState(scene.data?.items);
-    }, [scene.data?.items]);
-
-    useWebsocket({
-        orgName: "finn",
-        onMessage(data) {
-            setState((prev) => {
-                const itemToUpdate = prev?.find(
-                    (item) => item.id === data.objectId,
-                );
-                if (!itemToUpdate || !prev) {
-                    return prev;
-                }
-                itemToUpdate[data.path as keyof SceneItem] = data.value as any;
-                return [...prev];
-            });
-        },
-    });
     const sphereRef = useRef<Group | null>(null);
+
+    useFrame(() => {
+        const sphere = sphereRef.current;
+        if (sphere) {
+            sphere.rotation.y += Math.PI / 500;
+        }
+    });
 
     return (
         <>
             <ambientLight intensity={0.5} />
             <directionalLight position={[0, 0, 5]} />
             <group ref={sphereRef}>
-                {sceneItems?.map((item) => {
+                {scene.data?.items.map((item) => {
                     switch (item.meshType) {
                         case "Cube":
                             return (
@@ -100,7 +44,7 @@ function Scene() {
                                     position={item.position}
                                 >
                                     <boxGeometry args={[1]} />
-                                    <meshStandardMaterial color="#F11FF1" />
+                                    <meshStandardMaterial color="red" />
                                 </mesh>
                             );
                         case "Sphere":
