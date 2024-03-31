869e32e821d64bf603230c9be319617504223c8e
Finn <34718806+FinnDore@users.noreply.github.com>
2024-03-31 16:34:57 +0100

    .

--- apps/relay/src/game_socket.rs
+++ apps/relay/src/game_socket.rs
@@ -1,5 +1,5 @@
 use crate::{
-    scene::{self, SceneUpdate},
+    scene::{self, create_test_scene, SceneUpdate},
     util::ErrorFormatter,
     SharedState,
 };
@@ -10,8 +10,9 @@ use axum::{
     },
     http::{status, HeaderMap},
     response::IntoResponse,
+    Error,
 };
-use rand::{random, seq::SliceRandom, Rng};
+use rand::Rng;
 use tokio::time::sleep;
 use tracing::{error, info};
 
@@ -46,38 +47,44 @@ pub async fn game_handler(
     ws.on_upgrade(|socket| handle_game_socket(socket, org_id, state))
 }
 
-pub async fn handle_game_socket(mut socket: WebSocket, org_id: String, state: SharedState) {
+async fn handle_game_socket(mut socket: WebSocket, org_id: String, state: SharedState) {
+    let state_gaurd = state.read().await;
+    let is_simulation = state_gaurd.simulation.clone();
+    drop(state_gaurd);
+
+    let mut sim = Simualtion {
+        scene: create_test_scene(),
+    };
+    println!("Simulation: {:?}", is_simulation);
+
     loop {
-        // while let Some(msg) = socket.recv().await {
-        // if let Err(err) = msg {
-        //     error!("Error receiving message {}", err);
-        //     return;
-        // }
+        let msg = match is_simulation {
+            true => recv_simulation_frame(&mut sim).await,
+            false => socket.recv().await,
+        };
+
+        if msg.is_none() {
+            info!(
+                org_id,
+                "Cannot recive message from simulation or gameserver"
+            );
+            return;
+        };
+
+        let msg = msg.unwrap();
+        if let Err(err) = msg {
+            error!(
+                org_id,
+                error = ErrorFormatter::format_axum_error(err),
+                "Error receiving message from simulation or gameserver"
+            );
+            return;
+        }
+        let update_msg = msg.unwrap();
 
         let state_gaurd = &mut state.write().await;
         let mut current_orgs = state_gaurd.orgs.lock().await;
         if let Some(org) = current_orgs.get_mut(&org_id) {
-            let mut scene = state_gaurd.scene.write().await;
-            let mut rng = rand::thread_rng();
-            let item_index_to_update = rng.gen_range(0..scene.items.len());
-
-            let item_to_update = scene.items.get_mut(item_index_to_update);
-            if item_to_update.is_none() {
-                info!(org_id, "No items in scene");
-                return;
-            }
-
-            let item = item_to_update.unwrap();
-            item.rotation.1 += 0.2;
-            let update_msg = Message::Text(
-                serde_json::to_string(&SceneUpdate {
-                    object_id: item_index_to_update.to_string(),
-                    path: "rotation".into(),
-                    value: item.rotation,
-                })
-                .unwrap(),
-            );
-
             for client in &mut org.clients {
                 if let Err(err) = client.tx.send(update_msg.clone()) {
                     error!(
@@ -88,9 +95,38 @@ pub async fn handle_game_socket(mut socket: WebSocket, org_id: String, state: Sh
                     );
                 }
             }
-        } else {
-            info!(org_id, "Cannot send message no connected clients found");
         }
-        sleep(std::time::Duration::from_millis(5)).await;
     }
+}
+
+struct Simualtion {
+    scene: scene::Scene,
+}
+
+async fn recv_simulation_frame(simulation: &mut Simualtion) -> Option<Result<Message, Error>> {
+    let item_index_to_update = {
+        let mut rng = rand::thread_rng();
+        rng.gen_range(0..simulation.scene.items.len())
+    };
+    let item_to_update = simulation.scene.items.get_mut(item_index_to_update);
+
+    if item_to_update.is_none() {
+        sleep(std::time::Duration::from_millis(5)).await;
+        return Some(Err(Error::new("No items in scene")));
+    }
+
+    let item = item_to_update.unwrap();
+    item.rotation.1 += 0.2;
+    item.rotation.2 += 0.2;
+    let update_msg = Message::Text(
+        serde_json::to_string(&SceneUpdate {
+            object_id: item_index_to_update.to_string(),
+            path: "rotation".into(),
+            value: item.rotation,
+        })
+        .unwrap(),
+    );
+
+    sleep(std::time::Duration::from_millis(50)).await;
+    Some(Ok(update_msg))
 }
