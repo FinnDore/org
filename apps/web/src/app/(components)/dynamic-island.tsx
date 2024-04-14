56cb92af71e7caf08e34698a5807e9328d003043
Finn <34718806+FinnDore@users.noreply.github.com>
2024-04-14 19:58:40 +0100

    .

---
+++ apps/web/src/app/(components)/dynamic-island.tsx
@@ -0,0 +1,36 @@
+import { useRef } from "react";
+import { create } from "zustand";
+
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+
+type DynamicIslandState = {
+    orgName: string;
+    setOrgName: (orgName: string) => void;
+};
+const useStore = create<DynamicIslandState>((set) => ({
+    orgName: "verigoers",
+    setOrgName: (orgName: string) => set(() => ({ orgName })),
+}));
+
+export function DynamicIsland() {
+    const orgName = useStore();
+
+    const divRef = useRef<HTMLDivElement>(null);
+    const [parent] = useAutoAnimate({
+        duration: 500,
+        disrespectUserMotionPreference: true,
+    });
+    return (
+        <div ref={parent}>
+            <div
+                onClick={() => {
+                    orgName.setOrgName("largeeeeeeeaaaaaaaaa");
+                }}
+                className="rounded-full bg-black p-12 py-2 text-white shadow-xl dark:bg-white dark:text-black"
+                id="dyamicIsland"
+            >
+                {orgName.orgName}
+            </div>
+        </div>
+    );
+}
